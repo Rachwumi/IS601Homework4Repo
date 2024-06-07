91a540c0d49880e9cb49df488e4b81592e0349c1
Rachwumi <rao22@njit.edu>
2024-06-07 18:26:08 -0500

    both history file and test file pass all tests and code coverage

--- calculator/History.py
+++ calculator/History.py
@@ -2,13 +2,11 @@ from calculator.calculation import Calculation as calc
 
 class Calculator_History:
     log: list[calc] = []
-    size = 0
 
     @classmethod
     def addCalculation(cls, Calc):
-        cls.size += 1
         return cls.log.append(Calc)
     
     @classmethod
     def getLastCalculation(cls):
-        return cls.log[cls.size-1]
+        return cls.log[len(cls.log)-1]
