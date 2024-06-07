b66de1428d5d111d68c2921f789cb35be5765e7e
Rachwumi <rao22@njit.edu>
2024-06-07 14:21:41 -0500

    Added new test cases to the test_calculator file

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,10 +1,23 @@
 '''My Calculator Test'''
-from calculator import add, subtract
+from decimal import Decimal
+from calculator import Calculator
 
 def test_addition():
     '''Test that addition function works '''    
-    assert add(2,2) == 4
+    assert Calculator.add(2,2) == Decimal(4)
 
 def test_subtraction():
     '''Test that addition function works '''    
-    assert subtract(2,2) == 0
+    assert Calculator.subtract(2,2) == Decimal(0)
+
+def test_multiplication():
+    '''Test that addition function works '''    
+    assert Calculator.multiply(2,2) == Decimal(4)
+
+def test_division():
+    '''Test that addition function works '''    
+    assert Calculator.divide(2,2) == Decimal(1) 
+
+def test_divisionFailure():
+    '''Test that addition function works '''    
+    assert Calculator.subtract(2,2) == Decimal(0)
